66b4a042d58c1e9726d3de6c499f1943ba08d431
Samidha Dhawale <samidhadd21@gmail.com>
2026-05-11 19:00:59 +0530

    added tools file

---
+++ src/components/folders/Tools.jsx
@@ -0,0 +1,32 @@
+import RetroWindow from "./RetroWindow";
+
+const Tools = ({ onClose }) => {
+  return (
+    <RetroWindow
+      title="tools.txt"
+      onClose={onClose}
+      defaultPos={{ x: 180, y: 150 }}
+    >
+      <h1 className="text-sm font-bold text-[#351303] ml-2 mt-1">Weapons of choice! The stack behind my work.</h1>
+      <div className="flex gap-3 mt-1 mb-2 gap-4 ml-2 px-4 py-2">
+        <img src="" alt="html"/>
+        <img src="" alt="css"/>
+        <img src="" alt="js"/>
+        <img src="" alt="react"/>
+        <img src="" alt="tailwind"/>
+        <img src="" alt="mongodb"/>
+        <img src="" alt="sql"/>
+        <img src="" alt="aws"/>
+        <img src="" alt="vercel"/>
+        <img src="" alt="render"/>
+        <img src="" alt="nodejs"/>
+        <img src="" alt="cpp"/>
+        <img src="" alt="python"/>
+        <img src="" alt="github"/>
+        <img src="" alt="vscode"/>
+      </div>
+    </RetroWindow>
+  );
+};
+
+export default Tools;
--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -5,17 +5,20 @@ import AboutMe    from '../folders/Aboutme';
 import Connect    from '../folders/Connect';
 import Music      from '../folders/Music';
 import Resume     from '../folders/Resume';
+import Tools      from '../folders/Tools';
 
 import folderAbout    from '../../assets/folder-about.png';
 import folderMusic    from '../../assets/folder-music.png';
 import folderConnect  from '../../assets/folder-connect.png';
 import folderResume   from '../../assets/folder-resume.png';
+import folderTools from '../../assets/folder-resume.png';
 
 const folders = [
   { id: 'about',   label: 'About Me', icon: folderAbout,   pos: { top: '80px',  left: '60px'  } },
   { id: 'music',   label: 'My Playlist',    icon: folderMusic,   pos: { top: '220px', left: '60px'  } },
   { id: 'connect', label: 'Connect',  icon: folderConnect, pos: { top: '220px', right: '60px' } },
   { id: 'resume',  label: 'Resume',   icon: folderResume,  pos: { top: '340px', right: '60px' } },
+  { id: 'tools', label: 'Tools', icon:folderTools, pos: { top: '460px', right: '60px' }},
 ];
 
 const HomePage = () => {
@@ -28,6 +31,7 @@ const HomePage = () => {
       case 'music':    return <Music    onClose={close} />;
       case 'connect':  return <Connect  onClose={close} />;
       case 'resume':   return <Resume   onClose={close} />;
+      case 'tools':   return <Tools   onClose={close} />;
       default: return null;
     }
   };
