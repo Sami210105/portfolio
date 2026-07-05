9298fee1f4fd8fe31b1294c085b33b3eef74af59
Sami210105 <samidhadd21@gmail.com>
2026-07-05 23:37:14 +0530

    added ml toolkit images

--- src/components/folders/Tools.jsx
+++ src/components/folders/Tools.jsx
@@ -15,11 +15,11 @@ const Tools = ({ onClose }) => {
         <div>
           <h3 className="text-md font-bold mb-3 text-[#351303]">Frontend</h3>
           <div className="flex gap-4 flex-wrap">
-            <img src="tools/html.png" alt="html" className="w-14 h-auto object-contain"/>
-            <img src="tools/css.png" alt="css" className="w-14 h-auto object-contain"/>
-            <img src="tools/js.png" alt="js" className="w-14 h-auto object-contain"/>
-            <img src="tools/react.png" alt="react" className="w-14 h-auto object-contain"/>
-            <img src="tools/tailwind.png" alt="tailwind" className="w-14 h-auto object-contain"/>
+            <img src="tools/html.png" alt="html" className="w-12 h-auto object-contain"/>
+            <img src="tools/css.png" alt="css" className="w-10 h-auto object-contain"/>
+            <img src="tools/js.png" alt="js" className="w-12 h-auto object-contain"/>
+            <img src="tools/react.png" alt="react" className="w-12 h-auto object-contain"/>
+            <img src="tools/tailwind.png" alt="tailwind" className="w-12 h-auto object-contain"/>
           </div>
         </div>
 
@@ -27,10 +27,10 @@ const Tools = ({ onClose }) => {
         <div>
           <h3 className="text-md font-bold mb-3 text-[#351303]">Backend & Database</h3>
           <div className="flex gap-4 flex-wrap">
-            <img src="tools/mongodb.png" alt="mongodb" className="w-14 h-auto object-contain"/>
-            <img src="tools/sql.png" alt="sql" className="w-14 h-auto object-contain"/>
-            <img src="tools/nodejs.png" alt="nodejs" className="w-14 h-auto object-contain"/>
-            <img src="tools/aws.png" alt="aws" className="w-14 h-auto object-contain"/>
+            <img src="tools/mongodb.png" alt="mongodb" className="w-12 h-auto object-contain"/>
+            <img src="tools/sql.png" alt="sql" className="w-12 h-auto object-contain"/>
+            <img src="tools/nodejs.png" alt="nodejs" className="w-12 h-auto object-contain"/>
+            <img src="tools/aws.png" alt="aws" className="w-12 h-auto object-contain"/>
           </div>
         </div>
 
@@ -38,21 +38,28 @@ const Tools = ({ onClose }) => {
         <div>
           <h3 className="text-md font-bold mb-3 text-[#351303]">Tools & Platforms</h3>
           <div className="flex gap-4 flex-wrap">
-            <img src="tools/vercel.png" alt="vercel" className="w-14 h-auto object-contain"/>
-            <img src="tools/render.png" alt="render" className="w-14 h-auto object-contain"/>
-            <img src="tools/cpp.png" alt="cpp" className="w-14 h-auto object-contain"/>
-            <img src="tools/python.png" alt="python" className="w-14 h-auto object-contain"/>
-            <img src="tools/github.png" alt="github" className="w-14 h-auto object-contain"/>
-            <img src="tools/vscode.png" alt="vscode" className="w-14 h-auto object-contain"/>
+            <img src="tools/vercel.png" alt="vercel" className="w-12 h-auto object-contain"/>
+            <img src="tools/render.png" alt="render" className="w-12 h-auto object-contain"/>
+            <img src="tools/cpp.png" alt="cpp" className="w-12 h-auto object-contain"/>
+            <img src="tools/python.png" alt="python" className="w-12 h-auto object-contain"/>
+            <img src="tools/github.png" alt="github" className="w-12 h-auto object-contain"/>
+            <img src="tools/vscode.png" alt="vscode" className="w-12 h-auto object-contain"/>
           </div>
         </div>
 
-        <div className="flex gap-4 flex-wrap">
-          <img src="tools/numpy.png" alt="NumPy" className="w-14 h-auto object-contain"/>
-          <img src="tools/pandas.png" alt="Pandas" className="w-14 h-auto object-contain"/>
-          <img src="tools/scikitlearn.png" alt="Scikit-learn" className="w-14 h-auto object-contain"/>
-          <img src="tools/transformers.png" alt="Transformers" className="w-14 h-auto object-contain"/>
-          <img src="tools/huggingface.png" alt="Hugging Face" className="w-14 h-auto object-contain"/>
+        {/* Recent ML Learnings */}
+        <div>
+          <h3 className="text-md font-bold mb-3 text-[#351303]">
+            ML toolkit
+          </h3>
+
+          <div className="flex gap-4 flex-wrap">
+            <img src="tools/transformer.png" alt="Transformers" className="w-14 h-auto object-contain"/>
+            <img src="tools/pytorch.png" alt="PyTorch" className="w-14 h-auto object-contain"/>
+            <img src="tools/numpy.png" alt="NumPy" className="w-14 h-auto object-contain"/>
+            <img src="tools/pandas.png" alt="Pandas" className="w-12 h-auto object-contain"/>
+            <img src="tools/sklearn.png" alt="Scikit-learn" className="w-14 h-auto object-contain"/>
+          </div>
         </div>
       </div>
     </RetroWindow>
